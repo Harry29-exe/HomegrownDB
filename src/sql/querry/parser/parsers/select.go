62a99ea010ee53227603aca82d4ebf56795aa265
Kamil Wójcik <wojcikakamil@gmail.com>
2022-04-06 00:25:25 +0200

    added breakSkipper for skipping multiple break tokens

--- src/sql/querry/parser/parsers/select.go
+++ src/sql/querry/parser/parsers/select.go
@@ -4,7 +4,7 @@ import (
 	"HomegrownDB/sql/querry/parser/defs"
 	"HomegrownDB/sql/querry/parser/helpers"
 	"HomegrownDB/sql/querry/parser/ptree"
-	"HomegrownDB/sql/querry/tokenizer"
+	"HomegrownDB/sql/querry/tokenizer/token"
 )
 
 var Select selectParser = selectParser{}
@@ -18,12 +18,20 @@ func (s selectParser) Parse(source defs.TokenSource) (ptree.Node, error) {
 
 	// Select
 	_, err := helpers.CurrentToken(source).
-		HasCode(tokenizer.Select).
+		HasCode(token.Select).
 		Check()
 	if err != nil {
 		return nil, err
 	}
 	s.Root = ptree.NewSelectNode()
+
+	err = helpers.SkipBreaks(source).
+		TypeMax(token.SpaceBreak, 2).
+		TypeMinMax(token.Comma, 1, 1).
+		ShipFromNext()
+	if err != nil {
+		return nil, err
+	}
 
 	// Fields
 	err = s.Attach(Fields.Parse(source))
