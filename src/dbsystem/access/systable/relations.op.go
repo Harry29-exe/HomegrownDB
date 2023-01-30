32ba1bf61fbe40e66e721d7f81cf589377c6abe7
Kamil Wójcik <wojcikakamil@gmail.com>
2023-01-30 08:03:36 +0100

    implemented TableAsRelationsRow method

--- src/dbsystem/access/systable/relations.op.go
+++ src/dbsystem/access/systable/relations.op.go
@@ -1,8 +1,12 @@
 package systable
 
 import (
+	"HomegrownDB/dbsystem/hgtype/inputtype"
+	"HomegrownDB/dbsystem/relation"
 	"HomegrownDB/dbsystem/relation/table"
 	"HomegrownDB/dbsystem/storage/page"
+	"HomegrownDB/dbsystem/tx"
+	"log"
 )
 
 var relationsDef = RelationsTableDef()
@@ -10,13 +14,30 @@ var relationsDef = RelationsTableDef()
 type RelationsOps struct {
 }
 
-func (o RelationsOps) TableAsRelationsTuple(table table.Definition) page.Tuple {
+func (o RelationsOps) TableAsRelationsRow(
+	table table.Definition,
+	tx tx.Tx,
+	commands uint16,
+) page.Tuple {
 	builder := page.NewTupleBuilder()
 	builder.Init(page.PatternFromTable(table))
-	//builder.WriteValue(hgtype.Value{
-	//	TypeTag:   inputtype.ConvInt8(0)),
-	//	NormValue: nil,
-	//}
-	//todo implement me
-	panic("Not implemented")
+
+	err := builder.WriteValue(inputtype.ConvInt8Value(int64(table.OID())))
+	if err != nil {
+		log.Printf("unexpected err: %s\n", err.Error())
+	}
+	err = builder.WriteValue(inputtype.ConvInt8Value(int64(relation.TypeTable)))
+	if err != nil {
+		log.Printf("unexpected err: %s\n", err.Error())
+	}
+	err = builder.WriteValue(inputtype.ConvInt8Value(int64(table.FsmOID())))
+	if err != nil {
+		log.Printf("unexpected err: %s\n", err.Error())
+	}
+	err = builder.WriteValue(inputtype.ConvInt8Value(int64(table.VmOID())))
+	if err != nil {
+		log.Printf("unexpected err: %s\n", err.Error())
+	}
+
+	return builder.VolatileTuple(tx, commands)
 }
